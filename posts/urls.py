d96e0b785a840ed138378be3785a644fd21e0ce1
whedia <hedia.wejdi94@gmail.com>
2020-09-14 16:28:41 +0200

    third commit

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from .views import HomePageView, CreatePostView, DeletePostView
+from .views import HomePageView, CreatePostView, DeletePostView, EditPostView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('post/', CreatePostView.as_view(), name='add_post'), 
-    path('delete/', DeletePostView.as_view(), name='delete_post'),
+    path('<int:pk>/delete/', DeletePostView.as_view(), name='delete_post'),
+    path('<int:pk>/edit/', EditPostView.as_view(), name='edit_post'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,18 +1,15 @@
 from django.shortcuts import render, redirect, get_object_or_404
-
-# Create your views here.
-
-from django.views.generic import ListView, CreateView, DeleteView # new
-from django.urls import reverse_lazy # new
-
-from .forms import PostForm # new
+from django.views.generic import ListView, CreateView, DeleteView 
+from django.views.generic.edit import UpdateView
+from django.urls import reverse_lazy 
+from .forms import PostForm 
 from .models import Post
 
 class HomePageView(ListView):
     model = Post
     template_name = 'home.html'
 
-class CreatePostView(CreateView): # new
+class CreatePostView(CreateView): 
     model = Post
     form_class = PostForm
     template_name = 'post.html'
@@ -23,3 +20,11 @@ class DeletePostView(DeleteView):
     form_class = PostForm
     template_name = 'confirm_delete.html'
     success_url = reverse_lazy('home')
+
+class EditPostView(UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'edit.html'
+    success_url = reverse_lazy('home')
+
+    
